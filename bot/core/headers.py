11c1d3cce5edc1c9afdb03bb7900af27e04476fa
Foad Davoodi <myf.davoodi@gmail.com>
2024-09-13 03:02:49 +0330

    Add files via upload

---
+++ bot/core/headers.py
@@ -0,0 +1,16 @@
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Content-Type': 'application/json',
+    'Connection': 'keep-alive',
+    'Origin': 'https://dev.goatsbot.xyz',
+    'Referer': 'https://dev.goatsbot.xyz/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'Sec-Ch-Ua-mobile': '?1',
+    'Sec-Ch-Ua-platform': '"Android"',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
+    'X-Requested-With': 'org.telegram.messenger'
+}
